b12fb89d9bd0286592d94374261fa851594cdbe1
Mosesosamede <89026312+Mosesosamede@users.noreply.github.com>
2026-01-17 10:47:41 +0100

    Update tailwind.config.js

--- Meta react-portfolio/tailwind.config.js
+++ Meta react-portfolio/tailwind.config.js
@@ -2,20 +2,20 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       animation: {
-        'cycling-bg': 'cycling-bg 3s linear infinite',
+        "cycling-bg": "cycling-bg 3s linear infinite",
       },
       keyframes: {
-        'cycling-bg': {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
+        "cycling-bg": {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
         },
       },
     },
   },
   plugins: [],
-}
+};
